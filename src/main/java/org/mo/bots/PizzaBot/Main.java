9b9f9b7106c24d267188802d6744ca9e4352c712
Code Master <makovskyi.alexander@gmail.com>
2021-09-17 13:55:08 +0300

    Add simple replying bot

--- src/main/java/org/mo/bots/PizzaBot/Main.java
+++ src/main/java/org/mo/bots/PizzaBot/Main.java
@@ -1,9 +1,23 @@
 package org.mo.bots.PizzaBot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
 
+    static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Config successful");
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new PizzaBot());
+            log.info("BOT INITIALIZED");
+        } catch (Exception e) {
+            log.error("BOT INITIALIZATION FAILED");
+            e.printStackTrace();
+        }
     }
 
 }
